69023f5a2cab805f09a44e3391b7ebe9ade6e1dd
cmb007 <2351580924@qq.com>
2019-04-22 14:30:22 +0800

    网易公开课首页

--- 163ted/js/main.js
+++ 163ted/js/main.js
@@ -2,16 +2,34 @@ window.onload = function() {
     var items = document.getElementById("nav2-main").getElementsByTagName("li");
     var itemParent = document.getElementById("nav2-main").getElementsByTagName("ul")[0];
     var body = document.getElementsByTagName("body")[0];
+    var imgList = document.getElementById("scroll-left-bottom").getElementsByTagName("img"); //滚动图列表
+    var scrollImg = document.getElementById("scroll-main");  //轮播图的容器
+    var scaleA = document.getElementById("today-detail").getElementsByTagName("a");  //今日特惠列表
+    var scaleImg = document.getElementById("today-detail").getElementsByTagName("img");  //缩放图
     var string = "";
     var timer = null;
-    //鼠标在项目栏项目上停留时的函数。
     
+    
+    //鼠标在项目栏项目上停留时的函数。
     for(var i = 0; i < 16; i++) {
         fun1(items[i], i);   
     }
-    
-
     //fun1(items[0]);
+
+    //右侧列表的显示函数
+    fun2();
+
+    //左侧滚动图片的函数
+    for(var i = 0; i < imgList.length; i++) {
+        fun3(imgList, i);
+    }
+
+    //图片缩放功能
+    for(var i = 0; i < scaleA.length; i++) {
+        fun4(scaleA[i], i);
+    }
+    
+    
 
     function fun1(item, index) {
         var itemDetail = document.createElement("div");
@@ -103,8 +121,64 @@ window.onload = function() {
 
         
     }
+
+    //右侧列表的函数
+    function fun2() {
+        var titleLi = document.getElementById("rightListTitle").getElementsByTagName("li");
+        var rightList1 = document.getElementById("rightList1");
+        var rightList2 = document.getElementById("rightList2");
+        
+        titleLi[0].onmouseover = function() {
+            this.style.borderBottom = "4px solid #286a46";
+            this.style.color = "#fff";
+            titleLi[1].style.borderBottom = "none";
+            titleLi[1].style.color = "#888";
+            rightList1.style.display = "block";
+            rightList2.style.display = "none";
+        }
+
+        titleLi[1].onmouseover = function() {
+            this.style.borderBottom = "4px solid #286a46";
+            this.style.color = "#fff";
+            titleLi[0].style.borderBottom = "none";
+            titleLi[0].style.color = "#888";
+            rightList1.style.display = "none";
+            rightList2.style.display = "block";
+        }
+    }
+
+    function fun3(imgItems, index) {
+        var scrollLabel = document.getElementById("scroll-label");
+        var scrollH1 = document.getElementById("scroll-left-text").getElementsByTagName("h1")[0];
+        var scrollP = document.getElementById("scroll-left-text").getElementsByTagName("p")[0];
+        for(var i = 0; i < imgItems.length; i++) {
+            if(i == index) {
+                imgItems[i].onmouseover = function() {
+                    this.parentNode.style.borderColor = "#286a46";
+                    scrollImg.style.backgroundImage = "url('images/scroll/"+ (index + 1).toString() + ".jpeg')";
+                    scrollLabel.innerHTML = leftScrollText[index].label;
+                    scrollLabel.style.backgroundColor = leftScrollText[index].labelColor;
+                    scrollH1.innerHTML = leftScrollText[index].h1;
+                    scrollP.innerHTML = leftScrollText[index].p;
+                    for(var j = 0; j < imgItems.length; j++) {
+                        if(j != index) {
+                            imgItems[j].parentNode.style.borderColor = "#999";
+                        }
+                    }
+                }
+            }
+        }
+        
+    }
     
-    
+    function fun4(obj, index) {
+        obj.onmouseover = function() {
+            scaleImg[index].style.transform = "scale(1.25)";
+        }
+        obj.onmouseout = function() {
+            scaleImg[index].style.transform = "scale(1)";
+        }
+    }
     
     
 }
